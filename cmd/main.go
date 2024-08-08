5810c75e8cda39031d17665fd293c8d2bd6ebc6f
Alban Bytyci <abytyqi@gmail.com>
2024-08-08 16:33:24 +0200

    add members model

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"GymEventTracker/internal/database"
 	"GymEventTracker/internal/routes"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	// Initialize the SQLite database
+	database.InitDB("gym_event_tracker.db")
+	defer database.CloseDB()
+
 	// Create a new Echo instance
 	e := echo.New()
 
 	// Setup routes
 	routes.SetupRoutes(e)
 
-	// Start the server on port 8080
-	e.Logger.Fatal(e.Start(":3000"))
+	// Graceful shutdown
+	go func() {
+		if err := e.Start(":3000"); err != nil {
+			e.Logger.Info("Shutting down the server...")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+
+	if err := e.Shutdown(nil); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -10,4 +10,11 @@ func SetupRoutes(e *echo.Echo) {
 	e.GET("/", handlers.Home)
 	e.GET("/events", handlers.ListEvents)
 	e.POST("/events", handlers.CreateEvent)
+
+	// Members routes
+	e.GET("/members", handlers.GetMembers)          // Retrieve all members
+	e.POST("/members", handlers.CreateMember)       // Add a new member
+	e.GET("/members/:id", handlers.GetMember)       // Retrieve a member by ID
+	e.PUT("/members/:id", handlers.UpdateMember)    // Update a member
+	e.DELETE("/members/:id", handlers.DeleteMember) // Delete a member
 }
